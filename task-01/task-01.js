8c7b7e8c7acf8e0f7edc69d87863087ac0e3aef3
Vitaliy-1915 <74369854+Vitaliy-1915@users.noreply.github.com>
2021-04-04 14:18:00 +0300

    добавил js

---
+++ task-01/task-01.js
@@ -0,0 +1,8 @@
+const itemEl = document.querySelectorAll('.item');
+console.log(itemEl);
+console.log('В списке',itemEl.length,'категории.');
+
+const textHeadEl = document.querySelectorAll('h2');
+console.log(textHeadEl);
+
+textHeadEl.forEach(element => textHeadEl.element);
