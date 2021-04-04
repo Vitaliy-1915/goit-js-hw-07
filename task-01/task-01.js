9d221276c7074feab455116ee938a0f61afa97c0
Vitaliy-1915 <74369854+Vitaliy-1915@users.noreply.github.com>
2021-04-04 19:22:22 +0300

    поправил код

--- task-01/task-01.js
+++ task-01/task-01.js
@@ -3,8 +3,8 @@ console.log('В списке', ElementItemLi.length, 'категории.');
 
 const elems = document.querySelectorAll('ul#categories li.item');
 elems.forEach(elem => {
-    console.log('Категория:',elem.children[0].textContent);
-    console.log('Количество элементов:',elem.children[1].children.length);
+    console.log('Категория:',elem.firstElementChild.textContent);
+    console.log('Количество элементов:',elem.lastElementChild.children.length);
 });
 
 
